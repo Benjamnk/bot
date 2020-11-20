ba46a8903ac6a8054862b03bf6b5751fdf2c65ae
Your Name <k.benjamink@gmail.com>
2020-11-21 04:12:08 +0700

    git remote add origin https://github.com/Benjamnk/bot.git

---
+++ project/config.py
@@ -0,0 +1,8 @@
+
+Channel_secret = "6cab98e968e5bcfece4a3c4ea11556ff"
+Channel_access_token  = 'd8ZyIL55JBF0mIpZAHkYVk+R7QZTtZdvLex4/dUQjEJl7PyS3CUqi390P9f6BMkLZ1gP1lwT9/uMfiThF7WyVNFvHBZb6Sfd3WUXM+bqOyiyeddKg40hM6YAj6rc3dduJR5TCZ3RcBQ4/zcVmvorbQdB04t89/1O/w1cDnyilFU=
+
+Bot_basic_id = '@848qbcbs'                         
+
+
+    
